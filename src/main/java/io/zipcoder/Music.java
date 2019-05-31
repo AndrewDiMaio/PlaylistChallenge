d38b6fd2d628b64ff10571c7d9e55885f375e2f8
andrewdimaio <andrewdimaio777@gmail.com>
2019-05-31 16:03:52 -0400

    finished Lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,40 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int countUp = 0;
+        int countDown = 0;
+        int count = 0;
+
+        for (int i = startIndex; i<playList.length; i++){
+            countUp++;
+        }
+
+        for (int j = startIndex; j>-1; j--){
+            countDown++;
+        }
+
+        if (countUp > countDown){
+            count = countDown;
+        }else{
+            count = countUp;
+        }
+
+
+        return count;
+
     }
 }
+
+
+
